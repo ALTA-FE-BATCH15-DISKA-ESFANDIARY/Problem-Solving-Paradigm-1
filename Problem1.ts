fa721f5f73e9c684ba8746f5f9fb3902cda0e1ee
Xpow22 <diskaesfandiary22@gmail.com>
2023-07-20 17:34:00 +0700

    memperbaiki No Solution

--- Problem1.ts
+++ Problem1.ts
@@ -1,9 +1,9 @@
-function solveEquations(A: number, B: number, C: number): [number, number, number] | string {
+function simpleEquations(A: number, B: number, C: number): [number, number, number] | string {
     for (let x = -10000; x <= 10000; x++) {
       for (let y = x + 1; y <= 10000; y++) {
         const z = A - x - y;
   
-        if (x * y * z === B && x ** 2 + y ** 2 + z ** 2 === C) {
+        if (x * y * z === B && Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2) === C) {
           return [x, y, z];
         }
       }
@@ -11,8 +11,7 @@ function solveEquations(A: number, B: number, C: number): [number, number, numbe
   
     return "No solution";
   }
-  
 
-  console.log(solveEquations(1, 2, 3));
-  console.log(solveEquations(6, 6, 14));
+  console.log(simpleEquations(1, 2, 3));
+  console.log(simpleEquations(6, 6, 14));
   
